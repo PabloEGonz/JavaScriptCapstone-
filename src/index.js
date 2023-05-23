a3179f2ad37cd9868e434b46010a3861a659f954
Pablo <ezekielgonzalezr@gmail.com>
2023-05-22 18:55:42 -0600

    Add series Id's

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 import logo from './img/LogoMN.png';
 
+const series = ['tt0944947', 'tt11198330', 'tt8772296', 'tt2661044', 'tt4574334', 'tt2085059',]
+
 const LOGO_HEADER = document.querySelector('.logo');
 LOGO_HEADER.src = logo;
+
+const container = document.querySelector('.landing-page');
+
+
