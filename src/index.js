293408b6f5b6c71de4d208c27d86b9f872634a5c
Pablo <ezekielgonzalezr@gmail.com>
2023-05-23 16:57:48 -0600

    Remove comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './style.css';
 import './popUp.css';
 import logo from './img/LogoMN.png';
-// import popUpRender from './modules/popUp.js';
 import callAPI from './modules/callAPI.js';
 
 const LOGO_HEADER = document.querySelector('.logo');
 LOGO_HEADER.src = logo;
 
 document.addEventListener('DOMContentLoaded', callAPI());
-
-// export default seeComments;
