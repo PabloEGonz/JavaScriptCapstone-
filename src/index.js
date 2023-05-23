feb60383facdb66c59d42cb584b3494833f3baa4
Pablo <ezekielgonzalezr@gmail.com>
2023-05-23 09:55:56 -0600

    Create new function to render elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
 import logo from './img/LogoMN.png';
-import display from './modules/display.js';
+import callAPI from './modules/callAPI.js';
 
 const series = ['tt0944947', 'tt11198330', 'tt8772296', 'tt2661044', 'tt4574334', 'tt2085059'];
 
 const LOGO_HEADER = document.querySelector('.logo');
 LOGO_HEADER.src = logo;
 
-document.addEventListener('DOMContentLoaded', () => display(series));
+document.addEventListener('DOMContentLoaded', () => callAPI(series));
---
+++ src/modules/render.js
@@ -0,0 +1,20 @@
+import heart from '../img/likes.png';
+import { arrayObj } from './getSeries.js';
+
+const container = document.querySelector('.landing-page');
+
+const render = async () => {
+  container.innerHTML = '';
+  arrayObj.forEach((ele) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `<img class="card-img" src='${ele.image.medium}'>
+        <div class='flex card-heading'><h4 class="card-title">${ele.name}</h4>
+        <img class="card-icon" src='${heart}'></div>
+        <p class="likes">Likes</p>
+        <button class="pop-up" type="button">Comments</button>
+        <button class="reserve" type="button">Reservations</button>`;
+    container.appendChild(card);
+  });
+};
+export default render;
