8a50c6a6f13d8d60780bd915abb6a193dbe2e3bf
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-23 18:15:21 -0400

    Better UI on popUp

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import './style.css';
 import './popUp.css';
 import logo from './img/LogoMN.png';
-import popUp_render from './modules/popUp.js';
+import popUpRender from './modules/popUp.js';
 import callAPI from './modules/callAPI.js';
-
-
-const series = ['tt0944947', 'tt11198330', 'tt8772296', 'tt2661044', 'tt4574334', 'tt2085059'];
 
 const LOGO_HEADER = document.querySelector('.logo');
 LOGO_HEADER.src = logo;
 
-document.addEventListener('DOMContentLoaded', () => callAPI(series));
+document.addEventListener('DOMContentLoaded', callAPI());
 
-const seeComments =  () => {
-    const buttons = Array.from(document.getElementsByClassName('popUp-bttn'));
+const seeComments = () => {
+  const buttons = Array.from(document.getElementsByClassName('popUp-bttn'));
 
-    buttons.forEach((button, index) => {
-        button.addEventListener('click', () => {
-            popUp_render(index);
-        });
-    })
-}
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      popUpRender(index);
+    });
+  });
+};
 
 export default seeComments;
--- src/modules/callAPI.js
+++ src/modules/callAPI.js
@@ -1,12 +1,12 @@
 import { getSeries } from './getSeries.js';
 import render from './render.js';
 
-const callAPI = (array) => {
-  array.forEach(async (ele, i) => {
+const series = ['tt0944947', 'tt11198330', 'tt8772296', 'tt2661044', 'tt4574334', 'tt2085059'];
+
+const callAPI = () => {
+  series.forEach(async (ele) => {
     await getSeries(`https://api.tvmaze.com/lookup/shows?imdb=${ele}`);
-    if (i === array.length - 1) {
-      render();
-    }
+    render();
   });
 };
 
