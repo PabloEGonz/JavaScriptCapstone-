aa680fffd1e2daac8e29b6a53c6849d8d3701183
Pablo <ezekielgonzalezr@gmail.com>
2023-05-23 09:56:19 -0600

    Edit funtion for better performance

--- src/modules/getSeries.js
+++ src/modules/getSeries.js
@@ -1,14 +1,8 @@
-import heart from '../img/likes.png';
+const arrayObj = [];
 
 const getSeries = async (file) => {
   const obj = await fetch(file);
   const response = await obj.json();
-  return `<img class="card-img" src='${response.image.medium}'>
-    <div class='flex card-heading'><h4 class="card-title">${response.name}</h4>
-    <img class="card-icon" src='${heart}'></div>
-    <p class="likes">Likes</p>
-    <button class="pop-up" type="button">Comments</button>
-    <button class="reserve" type="button">Reservations</button>`;
+  arrayObj.push(response);
 };
-
-export default getSeries;
+export { arrayObj, getSeries };
