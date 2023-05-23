0cf28a9060b29ff7f0718aea59e314426b9fd06d
Pablo <ezekielgonzalezr@gmail.com>
2023-05-23 10:02:10 -0600

    Fix linters

--- src/modules/render.js
+++ src/modules/render.js
@@ -4,17 +4,17 @@ import { arrayObj } from './getSeries.js';
 const container = document.querySelector('.landing-page');
 
 const render = async () => {
-    container.innerHTML = '';
-    arrayObj.forEach((ele) => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = `<img class="card-img" src='${ele.image.medium}'>
+  container.innerHTML = '';
+  arrayObj.forEach((ele) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `<img class="card-img" src='${ele.image.medium}'>
         <div class='flex card-heading'><h4 class="card-title">${ele.name}</h4>
         <img class="card-icon" src='${heart}'></div>
         <p class="likes">Likes</p>
         <button class="pop-up" id="${ele.id}" type="button">Comments</button>
         <button class="reserve" type="button">Reservations</button>`;
-        container.appendChild(card);
-    });
+    container.appendChild(card);
+  });
 };
 export default render;
