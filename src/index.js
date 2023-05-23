a2a7b983088f5841d71f22ba5e11bf227e21b595
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-22 23:06:32 -0400

    Popup basic setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './style.css';
 import logo from './img/LogoMN.png';
+import popUp_render from './popUp.js';
 
 const LOGO_HEADER = document.querySelector('.logo');
 LOGO_HEADER.src = logo;
+
+const commentBttn = document.querySelector('.testbttn');
+
+commentBttn.addEventListener('click', popUp_render);
