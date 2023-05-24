4924a777000e4fe93123d8cab83bd7da4879ef40
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-24 12:20:15 -0400

    UPDATE ID for API in counter

--- src/modules/commentsCounter.js
+++ src/modules/commentsCounter.js
@@ -1,6 +1,6 @@
 const commentCounter = async (commentId) => {
     const response = await fetch(
-      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/o5YLWfucVuUSY3jbCcrI/comments?item_id=${commentId}`,
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/uVDPVhxCZlYlibEV1MxD/comments?item_id=${commentId}`,
     );
     if (response.status === 400) {
       return 0;
