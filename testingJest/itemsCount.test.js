6cfa9d3e6e37aef190d3bf68a5d5c0c4c106d463
Pablo <ezekielgonzalezr@gmail.com>
2023-05-24 11:40:04 -0600

    Create tests for items counter

---
+++ testingJest/itemsCount.test.js
@@ -0,0 +1,16 @@
+import itemCounter from "./itemsCounter.js"
+
+describe('Items Counter', () => {
+    test('Tests when the array has some value', () => {
+        const items = [{ item: 1 }, { item: 2 }, { item: 3 },];
+        expect(itemCounter(items)).toBe(3);
+    });
+    test('Test when the array is empty', () => {
+        const items = [];
+        expect(itemCounter(items)).toBe(0);
+    });
+    test('Test when the array has one value', () => {
+        const items = [{}];
+        expect(itemCounter(items)).toBe(1);
+    });
+})
